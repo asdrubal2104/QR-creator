07f3a0aef7433275dfd0d67d51a6173a396f3bbc
asdrubal2104 <asdrubalgaitan2104@gmail.com>
2024-05-15 12:24:35 -0600

    Generador de QR

---
+++ script.js
@@ -0,0 +1,50 @@
+document.getElementById('generateBtn').addEventListener('click', function() {
+  var link = document.getElementById('linkInput').value;
+  if (link) {
+      var qrcodeContainer = document.getElementById('qrcode');
+   
+      // Limpiar cualquier QR previo, pero mantener el contenedor
+      while (qrcodeContainer.firstChild) {
+        qrcodeContainer.removeChild(qrcodeContainer.firstChild);
+      }
+
+      // Configurar el QR Code Styling con alta resolución
+      var qrCode = new QRCodeStyling({
+          width: 2000,
+          height: 2000,
+          data: link,
+          image: '', // Ruta a un logo de prueba
+          imageOptions: {
+              crossOrigin: "anonymous",
+              margin: 20 // Ajustar margen del logo
+          },
+          dotsOptions: {
+              color: "#000000",
+              type: "rounded" // Opciones: square, dots, rounded, etc.
+          },
+          cornersSquareOptions: {
+              color: "#000000",
+              type: "extra-rounded" // Opciones: square, extra-rounded, etc.
+          },
+          cornersDotOptions: {
+              color: "#000000",
+              type: "dot" // Opciones: square, dot
+          },
+          backgroundOptions: {
+              color: "#ffffff",
+          }
+      });
+
+      qrCode.append(qrcodeContainer);
+
+      // Configurar el enlace para descargar la imagen del código QR
+      qrCode.getRawData('png').then(blob => {
+          const url = URL.createObjectURL(blob);
+          var downloadLink = document.getElementById('downloadLink');
+          downloadLink.href = url;
+          downloadLink.style.display = 'block';
+      });
+  } else {
+      alert("Por favor, introduce un enlace válido.");
+  }
+});
